029dfdb83d758f85a041763facf741b3a93bcd9d
Naman Srivastava <srivastava.naman511@gmail.com>
2021-02-26 23:33:50 +0530

    more tests

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ require("dotenv").config();
 const fetch = require("node-fetch");
 const TelegramBot = require("node-telegram-bot-api");
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+token = process.env.BOT_TOKEN;
+
+// const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+if (process.env.NODE_ENV === "production") {
+  bot = new TelegramBot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+} else {
+  bot = new TelegramBot(token, { polling: true });
+}
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
