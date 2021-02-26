8928545ee81a7d87436c0213ef2c4f664a3153f1
Naman Srivastava <srivastava.naman511@gmail.com>
2021-02-26 15:40:04 +0530

    integrated chatterbot api

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const fetch = require("node-fetch");
 const TelegramBot = require("node-telegram-bot-api");
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
@@ -17,11 +17,30 @@ bot.onText(/\/start/, (msg, match) => {
   bot.sendMessage(chatId, resp);
 });
 
-
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
-  if (msg.text == "dog") {
-    bot.sendMessage(chatId, "You sent 'dog'");
+const chatbotUrl = process.env.CHATTTERBOT_API_URL || "http://127.0.0.1:5000";
+bot.on("message", async (msg) => {
+  try {
+    const chatId = msg.chat.id;
+    if (msg.text.startsWith("/")) return;
+    //   if (msg.text == "dog") {
+    //     bot.sendMessage(chatId, "You sent 'dog'");
+    //   }
+    text = msg.text;
+    const body = { data: text };
+    const url = chatbotUrl + "/service";
+    const res = await fetch(url, {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    });
+    const json = await res.json();
+    const reply = json.text;
+    bot.sendMessage(chatId, reply);
+  } catch (error) {
+    console.error(error);
   }
 });
 
+bot.on("polling_error", (error) => {
+  console.log(error); // => 'EFATAL'
+});
