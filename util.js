215b02427b0b658a61b1fe1f5b83b63e096eb0ae
Naman Srivastava <srivastava.naman511@gmail.com>
2021-02-27 18:57:14 +0530

    added flag for switching between chatterbot and default

--- util.js
+++ util.js
@@ -2,23 +2,27 @@ const fetch = require("node-fetch");
 const Message = require("./models/message");
 
 exports.getResponse = async (msg) => {
-  //   text = msg.text;
-  //   const body = { data: text };
-  //   const chatbotUrl = process.env.CHATTTERBOT_API_URL || "http://127.0.0.1:5000";
+  let reply = "itterashai";
+  if (process.env.CHATTERBOT_STATUS == "on") {
+    text = msg.text;
+    const body = { data: text };
+    const chatbotUrl =
+      process.env.CHATTTERBOT_API_URL || "http://127.0.0.1:5000";
 
-  //   const url = chatbotUrl + "/service";
-  // console.log("url=", url);
-  // const res = await fetch(url, {
-  //   method: "post",
-  //   body: JSON.stringify(body),
-  //   headers: { "Content-Type": "application/json" },
-  // });
-  // const json = await res.json();
-  // const reply = json.text;
-
-  const test = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const testJson = await test.json();
-  const reply = testJson.title;
+    const url = chatbotUrl + "/service";
+    console.log("url=", url);
+    const res = await fetch(url, {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    });
+    const json = await res.json();
+    reply = json.text;
+  } else {
+    const test = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const testJson = await test.json();
+    reply = testJson.title;
+  }
 
   return reply;
 };
